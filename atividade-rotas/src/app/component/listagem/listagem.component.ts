227d57db742ff911d46a7496256f5f285eb83337
rodnei-c <rmcaetano04@gmail.com>
2024-09-19 12:03:54 -0300

    atividade angular rotas

--- atividade-rotas/src/app/component/listagem/listagem.component.ts
+++ atividade-rotas/src/app/component/listagem/listagem.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Funcionario } from '../../models/Funcionario';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-listagem',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './listagem.component.scss'
 })
 export class ListagemComponent {
+
+  @Input() funcionarios : Funcionario[] = [];
+  @Output() eventFuncEditar = new EventEmitter<Funcionario>();
+  @Output() eventFuncExcluir = new EventEmitter<Funcionario>();
+
+  constructor(private route: ActivatedRoute){
+    this.route.params.subscribe((valores) => {
+      console.log(valores);
+      const idFuncionario = valores['id'];
+      console.log(idFuncionario);
+    })
+  }
+
+  editarCliente(funcionario : any){
+    this.eventFuncEditar.emit(funcionario)
+  }
+
+  excluirCliente(funcionario : any){
+    this.eventFuncExcluir.emit(funcionario)
+  }
 
 }
